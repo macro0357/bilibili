a26c536bba49306b08733e97c15c80db48e49a87
Young <992337652@qq.com>
2023-07-12 12:23:27 +0800

    完善用户注册及登录

--- bilibili-api/src/main/java/com/bilibili/api/UserApi.java
+++ bilibili-api/src/main/java/com/bilibili/api/UserApi.java
@@ -3,13 +3,11 @@ package com.bilibili.api;
 import com.bilibili.api.support.UserSupport;
 import com.bilibili.domain.JsonResponse;
 import com.bilibili.domain.User;
+import com.bilibili.domain.UserInfo;
 import com.bilibili.service.UserService;
 import com.bilibili.service.util.RSAUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserApi {
@@ -43,5 +41,21 @@ public class UserApi {
     public JsonResponse<String> login(@RequestBody User user) throws Exception{
         String token = userService.login(user);
         return new JsonResponse<>(token);
+    }
+
+    @PutMapping("/user")
+    public JsonResponse<String> updateUsers(@RequestBody User user) throws Exception {
+        Long userId = userSupport.getCurrentUserId();
+        user.setId(userId);
+        userService.updateUsers(user);
+        return JsonResponse.success();
+    }
+
+    @PutMapping("/user-infos")
+    public JsonResponse<String> updateUserInfos(@RequestBody UserInfo userInfo) {
+        Long userId = userSupport.getCurrentUserId();
+        userInfo.setUserId(userId);
+        userService.updateUserInfos(userInfo);
+        return JsonResponse.success();
     }
 }
--- bilibili-dao/src/main/java/com/bilibili/dao/UserDao.java
+++ bilibili-dao/src/main/java/com/bilibili/dao/UserDao.java
@@ -3,6 +3,7 @@ package com.bilibili.dao;
 import com.bilibili.domain.User;
 import com.bilibili.domain.UserInfo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserDao {
@@ -15,4 +16,10 @@ public interface UserDao {
     User getUserById(Long id);
 
     UserInfo getUserInfoByUserId(Long userId);
+
+    User updateUsers(User user);
+
+    User getUserByPhoneOrEmail(@Param("phone") String phone, @Param("email") String email);
+
+    Integer updateUserInfos(UserInfo userInfo);
 }
