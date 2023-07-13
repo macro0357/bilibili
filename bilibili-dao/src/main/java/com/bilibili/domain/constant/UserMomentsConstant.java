ee2019c37a88e023cd1a5c03eecbf0e0bba5463e
Young <992337652@qq.com>
2023-07-13 21:23:46 +0800

    配合rabbitMQ实现动态推送

---
+++ bilibili-dao/src/main/java/com/bilibili/domain/constant/UserMomentsConstant.java
@@ -0,0 +1,9 @@
+package com.bilibili.domain.constant;
+
+public interface UserMomentsConstant {
+
+    public static final String GROUP_MOMENTS = "MomentsGroup";
+
+    public static final String TOPIC_MOMENTS = "Topic-Moments";
+
+}
