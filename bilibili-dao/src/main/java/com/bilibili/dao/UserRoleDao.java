09d36f4b2048858389d04f382cc4ef87923e6a32
Young <992337652@qq.com>
2023-07-13 22:58:35 +0800

    用户权限控制:页面访问权限和页面元素操作权限

---
+++ bilibili-dao/src/main/java/com/bilibili/dao/UserRoleDao.java
@@ -0,0 +1,12 @@
+package com.bilibili.dao;
+
+import com.bilibili.domain.auth.UserRole;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UserRoleDao {
+    public List<UserRole> getUserRoleByUserId(Long userId);
+
+}
---
+++ bilibili-service/src/main/java/com/bilibili/service/AuthRoleService.java
@@ -0,0 +1,28 @@
+package com.bilibili.service;
+
+import com.bilibili.domain.auth.AuthRoleElementOperation;
+import com.bilibili.domain.auth.AuthRoleMenu;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class AuthRoleService {
+
+    @Autowired
+    private AuthRoleElementOperationService authRoleElementOperationService;
+
+    @Autowired
+    private AuthRoleMenuService authRoleMenuService;
+
+
+    public List<AuthRoleElementOperation> getRoleElementOperationsByRoleIds(Set<Long> roleIdSet) {
+        return authRoleElementOperationService.getRoleElementOperationsByRoleIds(roleIdSet);
+    }
+
+    public List<AuthRoleMenu> getRoleMenusByRoleIds(Set<Long> roleIdSet) {
+        return authRoleMenuService.getRoleMenusByRoleIds(roleIdSet);
+    }
+}
