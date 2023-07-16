ca53d3ba1600744ff3955a6ac33beb4f7c028112
Young <992337652@qq.com>
2023-07-16 12:17:56 +0800

    文件秒传

--- bilibili-service/src/main/java/com/bilibili/service/FileService.java
+++ bilibili-service/src/main/java/com/bilibili/service/FileService.java
@@ -1,9 +1,15 @@
 package com.bilibili.service;
 
+import com.bilibili.dao.FileDao;
+import com.bilibili.domain.File;
 import com.bilibili.service.util.FastDFSUtil;
+import com.bilibili.service.util.MD5Util;
+import io.netty.util.internal.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
 
 @Service
 public class FileService {
@@ -11,10 +17,30 @@ public class FileService {
     @Autowired
     private FastDFSUtil fastDFSUtil;
 
+    @Autowired
+    private FileDao fileDao;
+
     public String uploadFileBySlices(MultipartFile slice,
-                                     String fileMd5,
+                                     String fileMD5,
                                      Integer sliceNo,
                                      Integer totalSliceNo) throws Exception {
-        return fastDFSUtil.uploadFileBySlices(slice, fileMd5, sliceNo, totalSliceNo);
+        File dbFileMD5 = fileDao.getFileByMD5(fileMD5);
+        if(dbFileMD5 != null){
+            return dbFileMD5.getUrl();
+        }
+        String url = fastDFSUtil.uploadFileBySlices(slice, fileMD5, sliceNo, totalSliceNo);
+        if(!StringUtil.isNullOrEmpty(url)){ //因为fastDFSUtil中传输最后一个slice完成才会返回url
+            dbFileMD5 = new File();
+            dbFileMD5.setCreateTime(new Date());
+            dbFileMD5.setMd5(fileMD5);
+            dbFileMD5.setUrl(url);
+            dbFileMD5.setType(fastDFSUtil.getFileType(slice));
+            fileDao.addFile(dbFileMD5);
+        }
+        return url;
+    }
+
+    public String getFileMD5(MultipartFile file) throws Exception {
+        return MD5Util.getFileMD5(file);
     }
 }
