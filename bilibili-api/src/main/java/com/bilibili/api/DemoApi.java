71cfaba0ecb8005c0337b2784c657c144f8cc785
Young <992337652@qq.com>
2023-07-16 11:31:38 +0800

    fastDFS+nginx: 文件分片与断点续传

--- bilibili-api/src/main/java/com/bilibili/api/DemoApi.java
+++ bilibili-api/src/main/java/com/bilibili/api/DemoApi.java
@@ -1,9 +1,11 @@
 package com.bilibili.api;
 
 import com.bilibili.service.DemoService;
+import com.bilibili.service.util.FastDFSUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
@@ -12,8 +14,16 @@ public class DemoApi {
     @Autowired
     private DemoService demoService;
 
+    @Autowired
+    private FastDFSUtil fastDFSUtil;
+
     @GetMapping("/query")
     public Map<String, Object> query(Long id) {
         return demoService.query(id);
+    }
+
+    @GetMapping("/slice")
+    public void slices(MultipartFile file) throws Exception {
+        fastDFSUtil.convertFileToSlices(file);
     }
 }
